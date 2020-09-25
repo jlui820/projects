b36ed2459a81e18ba92288ad0c1509695b5745e0
Jeff <jlui820@gmail.com>
2020-09-24 19:08:12 -0700

    create chat and sidebar components

---
+++ whatsapp-mern/src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <h1>I am a Sidebar</h1>
+    </div>
+  )
+}
+
+export default Sidebar;
+
+
